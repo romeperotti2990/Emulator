82eac62f59260872403fad4126c1de6508b75770
romeperotti2990 <meepug0@gmail.com>
2025-12-01 09:22:59 -0700

    Use environment PORT variable for production

--- server.js
+++ server.js
@@ -262,6 +262,7 @@ app.get('/api/proxy-rom', async (req, res) => {
 });
 
 // --- Server Start ---
-app.listen(3001, () => {
-    console.log('Server running on http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
